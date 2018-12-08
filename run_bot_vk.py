1c8f842f04907204859fc016c1a0228ec62cca25
V <veronika@g.c>
2018-12-08 16:15:50 +0100

    fix

--- run_bot_vk.py
+++ run_bot_vk.py
@@ -3,8 +3,10 @@ from config import *
 import vk_api
 import requests
 
+
 def write_msg(user_id, text):
-     vk_bot.method('messages.send', {'user_id': user_id, 'message': text})
+    vk_bot.method('messages.send', {'user_id': user_id, 'message': text})
+
 
 vk_bot = vk_api.VkApi(token=TOKEN)
 long_poll = vk_bot.method('messages.getLongPollServer', {'need_pts': 1, 'lp_version': 3})
@@ -17,7 +19,7 @@ print(str(long_poll))
 while True:
     long_poll = requests.get(
         'https://{server}?act={act}&key={key}&ts={ts}&wait=2500'.format(server=server, act='a_check', key=key,
-                                                                      ts=ts)).json()
+                                                                        ts=ts)).json()
 
     update = long_poll['updates']
     if update[0][0] == 4:
@@ -31,4 +33,3 @@ while True:
               str(update[0][6]))  # msg to us
 
     ts = long_poll['ts']
-
