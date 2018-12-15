c83dea20a6280ce374c8df84d63e87a181288501
RedCar1 <mr.seray2016@yandex.ru>
2018-12-15 18:12:05 +0300

    f

--- run_bot_vk.py
+++ run_bot_vk.py
@@ -1,13 +1,17 @@
 from config import *
 ###import vk
+
 import vk_api
 import requests
 import random
 
 
 def write_msg(user_id, text):
-    vk_bot.method('messages.send', {'user_id': user_id, 'message': text})
+    vk_bot.method('messages.send', {'user_id': user_id, 'message': text, 'random_id': random.randint(0, 1000)})
 
+
+def sprosit_zadanie(urok):
+    write_msg(user_id, 'Привет, ' + (user_name[0]['first_name']) + ', что задано по ' + urok)
 
 vk_bot = vk_api.VkApi(token=TOKEN)
 long_poll = vk_bot.method('messages.getLongPollServer', {'need_pts': 1, 'lp_version': 3})
@@ -27,7 +31,7 @@ def write_msg_attach(user_id, text, att_url):
                   {'user_id': user_id,
                    'attachment': att_url,
                    'message': text,
-                   'random': random.randint(0, 1000)})
+                   'random_id': random.randint(0, 1000)})
 
 def get_last_post(owner_id, count, offset, filter): # wall.get
 	response = vk_bot_user.method('wall.get',
@@ -48,15 +52,22 @@ while True:
         print(update)
         user_id = update[0][3]
         user_name = vk_bot.method('users.get', {'user_ids': user_id})
-		if 'красив' in update[0][6]: # search for 'красив'
-			group_id = -35684707 # group id always starts from minus
-			post_id = get_last_post(group_id, 1, 1, 'owner')
-			attach = 'wall' + str(group_id) + ' ' + str(post_id) # make link to post
-			write_msg_attach(user_id, 'вот тебе красота', attach)
-        if 'картинк' in update[0][6]:
+        if 'Вероника' in (user_name[0]['first_name']):
+            write_msg(user_id, 'Привет, ' + (user_name[0]['first_name']) + ', что задано по математике?')
+        #elif 'Сергей' in (user_name[0]['first_name']):
+            #sprosit_zadanie('физике')
+        #if 'красив' in update[0][6]: # search for 'красив'
+        #    group_id = -35684707 # group id always starts from minus
+         #   post_id = get_last_post(group_id, 1, 1, 'owner')
+          #  attach = 'wall' + str(group_id) + ' ' + str(post_id) # make link to post
+           # write_msg_attach(user_id, 'вот тебе красота', attach)
+        elif 'картинк' in update[0][6]:
             write_msg_attach(user_id,
                              'вот тебе огненная картинка',
-                             'photo-171720905_456239025')
+                             'photo-171720905_456232035')
+        elif 'Что задано' in update[0][6]:
+            write_msg(user_id,
+                             'Извини, но я не знаю, что задано')
         else:
             write_msg(user_id, 'Привет, ' + (user_name[0]['first_name']))  # message to user
 
@@ -65,3 +76,4 @@ while True:
               str(update[0][6]))  # msg to us
 
     ts = long_poll['ts']
+
